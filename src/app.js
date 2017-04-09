82efe0a301b9a0f1583c9e64979a7a1904840c13
Juntao Qiu <juntao.qiu@gmail.com>
2017-04-09 18:07:37 +0800

    color the node

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ window.onload = () => {
           children: [
             {
               name: '王静源',
-              parent: '武汉办公室'
-            }
+              parent: '武汉办公室',
+            },
           ],
         },
         {
@@ -20,69 +20,83 @@ window.onload = () => {
           children: [
             {
               name: '邱俊涛',
-              parent: '西安办公室'
+              parent: '西安办公室',
             },
             {
               name: '赵孜洋',
-              parent: '西安办公室'
+              parent: '西安办公室',
             },
           ],
         },
       ],
     },
   ];
-  
+
   var margin = {top: 20, right: 120, bottom: 20, left: 120},
-      width = 960 - margin.right - margin.left,
-      height = 800 - margin.top - margin.bottom;
+    width = 960 - margin.right - margin.left,
+    height = 800 - margin.top - margin.bottom;
 
-  var i = 0,
-      duration = 750,
-      root;
+  var i = 0, duration = 750, root;
 
-  var tree = d3.layout.tree()
-      .size([height, width]);
+  var tree = d3.layout.tree().size([height, width]);
 
-  var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.right + margin.left)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3
+    .select('body')
+    .append('svg')
+    .attr('width', width + margin.right + margin.left)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
   root = twChina[0];
+
+  var color = d3.scale.category10();
 
   draw(root);
 
   function draw(source) {
-
     // Compute the new tree layout.
-    var nodes = tree.nodes(root).reverse(),
-     links = tree.links(nodes);
+    var nodes = tree.nodes(root).reverse(), links = tree.links(nodes);
 
     // Normalize for fixed-depth.
-    nodes.forEach(function(d) { d.y = d.depth * 180; });
+    nodes.forEach(function(d) {
+      d.y = d.depth * 180;
+    });
 
     // Declare the nodesâ€¦
-    var node = svg.selectAll("g.node")
-     .data(nodes, function(d) { return d.id || (d.id = ++i); });
+    var node = svg.selectAll('g.node').data(nodes, function(d, i) {
+      return d.id || (d.id = ++i);
+    });
 
     // Enter the nodes.
-    var nodeEnter = node.enter().append("g")
-     .attr("class", "node")
-     .attr("transform", function(d) {
-      return "translate(" + d.y + "," + d.x + ")"; });
+    var nodeEnter = node
+      .enter()
+      .append('g')
+      .attr('class', 'node')
+      .attr('transform', function(d) {
+        return 'translate(' + d.y + ',' + d.x + ')';
+      });
 
-    nodeEnter.append("circle")
-     .attr("r", 10)
-     .style("fill", "#fff");
+    nodeEnter
+      .append('circle')
+      .attr('r', 10)
+      .attr('stroke', function(d, i) {
+        return d.children || d._children ? 'steelblue' : color(i);
+      })
+      .style('fill', '#fff');
 
-    nodeEnter.append("text")
-     .attr("x", function(d) {
-      return d.children || d._children ? -15 : 15; })
-     .attr("dy", ".35em")
-     .attr("text-anchor", function(d) {
-      return d.children || d._children ? "end" : "start"; })
-     .text(function(d) { return d.name; })
-     .style("fill-opacity", 1);
+    nodeEnter
+      .append('text')
+      .attr('x', function(d) {
+        return d.children || d._children ? -15 : 15;
+      })
+      .attr('dy', '.35em')
+      .attr('text-anchor', function(d) {
+        return d.children || d._children ? 'end' : 'start';
+      })
+      .text(function(d) {
+        return d.name;
+      })
+      .style('fill-opacity', 1);
   }
-}
+};
